c7fecd1c0822773b060ceb3525c4ebc3f782967b
Clay Reimann <clayreimann@yahoo-inc.com>
2015-12-11 19:09:27 -0600

    Add actions
    Currently supports
    * refresh
    * feature-request

--- platform/chrome/chrome.js
+++ platform/chrome/chrome.js
@@ -4,11 +4,25 @@
  */
 import SuggestionService from '../../lib/search-service';
 
-const MORE_RESULTS = ' <dim>Append ">" to see the next page.</dim>'
-let CACHE = {};
+const MORE_RESULTS = ' <dim>Append ">" to see the next page.</dim>';
+const FEATURE_REQUEST = 'feature-request';
+const REFRESH = 'refresh';
+const COMMANDS = [
+  {
+    content: `!${FEATURE_REQUEST}`,
+    description: '!<match>feature-request</match> - Request a new feature for this extension'
+  },
+  {
+    content: `!${REFRESH}`,
+    description: '<dim>!</dim><match>refresh</match> - Refresh the list of repositories from github'
+  }
+];
 
-function initalizeSuggestionService() {
-  chrome.storage.sync.get(['githubs'], bindCallbacks);
+let CACHE = {};
+let SUGGESTER = null;
+
+function initializeExtension() {
+  chrome.storage.sync.get(['githubs'], initalizeSuggestionService);
 }
 
 function parseOptions(text) {
@@ -22,44 +36,65 @@ function parseOptions(text) {
     chars.pop()
   }
 
+  isMeta = chars[0] === '!';
+  if (isMeta) {
+    command = chars.slice(1).join('');
+  }
+
   text = chars.join('');
 
 
   return {
     isMeta,
+    command,
     page,
     text: chars.join('')
   }
 }
 
-function getSuggestFunction(suggester) {
-  return function(text, suggest) {
-    let page;
-    if (text.length < 1) { return; }
-    ({page, text} = parseOptions(text));
+function getRepoSuggestions(text) {
+  let suggestions = [];
 
-    let suggestions;
-    if (CACHE.hasOwnProperty(text)) {
-      suggestions = CACHE[text]
-    } else {
-      suggestions = suggester.getRepositoriesMatching(text)
-        .map(function(repo) {
-          let matches = repo.url.match(new RegExp(text, "i"));
-          return {
-            content: repo.name,
-            description: '<dim>Open</dim> <url>' + repo.url.slice(0, matches.index) +
-              '<match>' + matches[0] + '</match>' +
-              repo.url.slice(matches.index + matches[0].length) + '</url>'
-          };
-        });
-      CACHE[text] = suggestions;
-    }
-    console.log('returning ' + suggestions.length + ' suggestions');
-    console.log(suggestions.map((s) => s.content.length));
-    console.log(suggestions.map((s) => s.content));
+  if (CACHE.hasOwnProperty(text)) {
+    suggestions = CACHE[text]
+  } else {
+    suggestions = SUGGESTER.getRepositoriesMatching(text)
+      .map(function(repo) {
+        let matches = repo.url.match(new RegExp(text, "i"));
+        return {
+          content: repo.name,
+          description: '<dim>Open</dim> <url>' + repo.url.slice(0, matches.index) +
+            '<match>' + matches[0] + '</match>' +
+            repo.url.slice(matches.index + matches[0].length) + '</url>'
+        };
+      });
+    CACHE[text] = suggestions;
+  }
+  console.log('returning ' + suggestions.length + ' suggestions');
+  console.log(suggestions.map((s) => s.content.length));
+  console.log(suggestions.map((s) => s.content));
 
+  return suggestions;
+}
 
-    let description = `${suggestions.length} results for query <match>%s</match>.`
+function getSuggestions(text, suggest) {
+  if (SUGGESTER == null) { return; }
+  if (text.length < 1) { return; }
+
+  let page, isMeta, command;
+  ({
+    page,
+    isMeta,
+    command,
+    text,
+  } = parseOptions(text));
+
+  let suggestions, description;
+  if (isMeta) {
+    suggestions = COMMANDS.slice();
+    description = 'Execute a command';
+  } else {
+    suggestions = getRepoSuggestions(text)
     if (suggestions.length > 5) {
       description += ` Page ${page + 1} of ${Math.ceil(suggestions.length / 5)}. ${MORE_RESULTS}`
     }
@@ -67,52 +102,72 @@ function getSuggestFunction(suggester) {
     for (let i = 0; i < page && suggestions.length > 5; i++) {
       suggestions = suggestions.slice(5);
     }
-    if (suggestions.length && suggestions[0].content === text) {
-      description = `${suggestions[0].description}  |  ${description}`
-    }
-    chrome.omnibox.setDefaultSuggestion({description});
-    suggest(suggestions);
-  };
+
+    description = `${suggestions.length} results for query <match>%s</match>.`;
+  }
+
+  if (suggestions.length && suggestions[0].content === text) {
+    description = `${suggestions[0].description}  |  ${description}`
+  }
+  chrome.omnibox.setDefaultSuggestion({description});
+  suggest(suggestions);
 }
 
-function getCompletionFunction(suggester) {
-  return function(repoName, disposition) {
-    console.log(repoName + ' ' + disposition);
-    let url = suggester.getUrlForRepo(repoName)
-    if (disposition === 'currentTab') {
-      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.update(tabs[0].id, {url: url});
-      });
-    } else if (disposition === 'newForegroundTab') {
-      chrome.tabs.create({url: url, active: true});
-    } else if (disposition === 'newBackgroundTab') {
-      chrome.tabs.create({url: url});
-    }
+function suggestionAccepted(suggestion, disposition) {
+  if (SUGGESTER == null) { return; }
 
+  console.log(`Accepted: ${suggestion} ${disposition}`);
+  let {
+    isMeta,
+    command,
+    text
+  } = parseOptions(suggestion);
+  let url;
+  if (isMeta) {
+    switch (command) {
+      case REFRESH:
+        initializeExtension();
+        return;
+      case FEATURE_REQUEST:
+        url = 'https://github.com/clayreimann/github-search-extension/issues?q=is%3Aopen+is%3Aissue+label%3Aenhancement';
+        break;
+    }
+  } else {
+    url = SUGGESTER.getUrlForRepo(suggestion)
+  }
+
+  if (disposition === 'currentTab') {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      chrome.tabs.update(tabs[0].id, {url: url});
+    });
+  } else if (disposition === 'newForegroundTab') {
+    chrome.tabs.create({url: url, active: true});
+  } else if (disposition === 'newBackgroundTab') {
+    chrome.tabs.create({url: url});
   }
 }
 
-function bindCallbacks(settings) {
+function initalizeSuggestionService(settings) {
   let githubInstances = settings.githubs || [];
   if (githubInstances.length < 1) {
     return;
   }
-  let suggester = new SuggestionService(githubInstances);
   CACHE = {};
-  suggester.queryGithubsForRepositories();
-
-  chrome.omnibox.onInputChanged.addListener(getSuggestFunction(suggester));
-  chrome.omnibox.onInputEntered.addListener(getCompletionFunction(suggester));
+  SUGGESTER = new SuggestionService(githubInstances);
+  SUGGESTER.queryGithubsForRepositories();
 }
 
-initalizeSuggestionService();
+initializeExtension();
 chrome.omnibox.onInputStarted.addListener(function() {
   console.log('inputStarted');
   chrome.omnibox.setDefaultSuggestion({description: 'Type to search for a repository'});
 });
 
+chrome.omnibox.onInputChanged.addListener(getSuggestions);
+chrome.omnibox.onInputEntered.addListener(suggestionAccepted);
+
 chrome.runtime.onMessage.addListener(function(message) {
   if (message === 'settings-updated') {
-    initalizeSuggestionService();
+    initializeExtension();
   }
 });
